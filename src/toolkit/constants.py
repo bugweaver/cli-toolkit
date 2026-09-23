97fcf746bc5771a3d2fbded8aee56df74a98c0a6
bugweaver <anatoly.shevchenko@internet.ru>
2026-09-23 22:57:01 +0300

    constants: add length, mass and temperature units

--- src/toolkit/constants.py
+++ src/toolkit/constants.py
@@ -1,1 +1,15 @@
 OPERATORS: str = "+-*/"
+
+LENGTH_TO_METERS: dict[str, float] = {
+    "mm": 0.001,
+    "cm": 0.01,
+    "m": 1.0,
+    "km": 1000.0,
+}
+
+MASS_TO_GRAMS: dict[str, float] = {
+    "g": 1.0,
+    "kg": 1000.0,
+}
+
+TEMPERATURE_UNITS: set[str] = {"c", "f", "k"}
